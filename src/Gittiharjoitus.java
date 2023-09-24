f72b22e8b89753d28f00d8fe6f710df040695275
MikaVennola <mika.vennola@student.hamk.fi>
2023-09-24 15:17:45 +0300

    Siistitään koodia ja otetaan ylimääräinen scanneri pois

--- src/Gittiharjoitus.java
+++ src/Gittiharjoitus.java
@@ -1,36 +1,36 @@
 import java.util.Scanner;
 import java.util.Random;
-import java.util.Scanner;
+
 
 public class Gittiharjoitus {
 
 	public static void main(String[] args) {
 		
-		Random random = new Random ();
-		
+		Random random = new Random ();	
 		Scanner in = new Scanner(System.in);
+		
 		int [] guesses = new int [7];
 		int [] rightnumbers = new int [7];
 		int laskuri = 0;
 		// Arpoo numerot
-			for (int i = 0 ; i< rightnumbers.length ; i++) {
-			int numerot = random.nextInt(6);
-			System.out.print(numerot + " ");
-			rightnumbers [i] = numerot;
+		for (int i = 0 ; i< rightnumbers.length ; i++) {
+		int numerot = random.nextInt(6);
+		System.out.print(numerot + " ");
+		rightnumbers [i] = numerot;
 			
-			try {
-				Thread.sleep(1000);
+		try {
+			Thread.sleep(1000);
 			}
 			catch(InterruptedException ex){
 				Thread.currentThread().interrupt();
-			}
-			
-			}
-			for (int i = 0; i < 20; i++) {
+			}		
+		}
+		
+		for (int i = 0; i < 20; i++) {
 				System.out.println();
 			}
 			// Käyttäjän arvaukset
-			for (int i = 0 ; i< guesses.length ; i++) {
+		for (int i = 0 ; i< guesses.length ; i++) {
 				System.out.println("Syötä " + (i+1) + ". numero");
 				int vastaus = Integer.parseInt(in.nextLine());
 				guesses [i] = vastaus;
@@ -40,18 +40,22 @@ public class Gittiharjoitus {
 		for (int i = 0; i<guesses.length ; i++) {
 		System.out.print(guesses[i] + " ");
 		}
+		
 		System.out.println();
 		System.out.println("Oikeat numerot: ");
 		for (int i = 0; i<rightnumbers.length ; i++) {
 			System.out.print(rightnumbers[i] + " ");
 			}
 		
+		// laskee oikeat vastaukset
 		for (int i = 0 ; i < guesses.length; i++) {
 		if (rightnumbers[i] == guesses[i]) {
 			laskuri = laskuri + 1;
 		}
-	
+		// tulostaa oikeat vastaukset & laskee kuinka monta prosenttia
+		// pelissä meni oikein
 		}
+		
 		float prosentti = (laskuri*100)/guesses.length;
 		System.out.println();
 		System.out.println("Oikeita numeroita oli: " + laskuri);
