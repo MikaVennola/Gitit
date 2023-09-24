2ed9a0cdfa35d350c5a3bd47f4beb068aac3b1b3
MikaVennola <mika.vennola@student.hamk.fi>
2023-09-24 15:02:50 +0300

    Initial commit

---
+++ src/Gittiharjoitus.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Gittiharjoitus {
+
+	public static void main(String[] args) {
+		
+		Random random = new Random ();
+		
+		Scanner in = new Scanner(System.in);
+		int [] guesses = new int [7];
+		int [] rightnumbers = new int [7];
+		int laskuri = 0;
+		// arpoo numerot
+			for (int i = 0 ; i< rightnumbers.length ; i++) {
+			int numerot = random.nextInt(6);
+			System.out.print(numerot + " ");
+			rightnumbers [i] = numerot;
+			try {
+				Thread.sleep(1000);
+			}
+			catch(InterruptedException ex){
+				Thread.currentThread().interrupt();
+			}
+			
+			}
+			for (int i = 0; i < 20; i++) {
+				System.out.println();
+			}
+			// Käyttäjän vastaukset
+			for (int i = 0 ; i< guesses.length ; i++) {
+				System.out.println("Syötä " + (i+1) + ". numero");
+				int vastaus = Integer.parseInt(in.nextLine());
+				guesses [i] = vastaus;
+			}
+		// Tulostaa käyttäjän arvaukset & oikeat numerot
+		System.out.println("Käyttäjän arvaukset: ");
+		for (int i = 0; i<guesses.length ; i++) {
+		System.out.print(guesses[i] + " ");
+		}
+		System.out.println();
+		System.out.println("Oikeat numerot: ");
+		for (int i = 0; i<rightnumbers.length ; i++) {
+			System.out.print(rightnumbers[i] + " ");
+			}
+		//  laskee oikeat vastaukset
+		for (int i = 0 ; i < guesses.length; i++) {
+		if (rightnumbers[i] == guesses[i]) {
+			laskuri = laskuri + 1;
+		}
+		// tulostaa oikeat vastaukset & laskee prosentit kuinka paljon oikein
+		}
+		float prosentti = (laskuri*100)/guesses.length;
+		System.out.println();
+		System.out.println("Oikeita numeroita oli: " + laskuri);
+		System.out.println("Prosenttia oikein pelistä: " + prosentti);
+	}
+}
